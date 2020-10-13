b50c21337138de7e45dae7ae08e0bbf0bf0c203f
Emily Holt <emily.holt@dartmouth.edu>
2020-10-13 10:25:48 -0400

    Returning y dataset based on split

--- problem_2/prob2_basics.py
+++ problem_2/prob2_basics.py
@@ -39,31 +39,38 @@ def split_into_train_and_valid(frac_valid=0.1428, random_state=None):
     # Load outcomes y arrays
     x_train = np.loadtxt(os.path.join(DATA_DIR, 'x_train.csv'), delimiter=',', skiprows=1)
     y_train = np.loadtxt(os.path.join(DATA_DIR, 'y_train.csv'), delimiter=',', skiprows=1)
-    print(y_train.shape)
-    total_samples = len(y_train)
-    total_positive_samples = np.count_nonzero(y_train == 1.0)
 
-    positive_samples = np.where(y_train == 1.0)[0]
-    negative_samples = np.where(y_train == 0)[0]
 
     # Determine the number of samples that will be in the test set
+    total_samples = len(y_train)
+    total_positive_samples = np.count_nonzero(y_train == 1.0)
+    positive_samples = np.where(y_train == 1.0)[0]
+    negative_samples = np.where(y_train == 0)[0]
     n_valid_samples = int(np.ceil(frac_valid * total_samples))
     n_valid_positive_samples = n_valid_samples // 2
 
+    # Get indices of positive samples
     positive_samples_copy = random_state.permutation(positive_samples)
     valid_positive_samples = positive_samples_copy[0:n_valid_positive_samples]
     train_positive_samples = positive_samples_copy[n_valid_positive_samples:]
 
+    # Get indices of negative samples
     negative_samples_copy = random_state.permutation(negative_samples)
     valid_negative_samples = negative_samples_copy[0:n_valid_positive_samples]
     train_negative_samples = negative_samples_copy[n_valid_positive_samples:]
 
-    train_set = np.vstack((x_train[train_positive_samples], x_train[train_negative_samples]))
-    valid_set = np.vstack((x_train[valid_positive_samples], x_train[valid_negative_samples]))
+    # Get the samples from the x & y dataset to make up trainset
+    x_train_set = np.vstack((x_train[train_positive_samples], x_train[train_negative_samples]))
+    y_train_set = np.hstack((y_train[train_positive_samples], y_train[train_negative_samples]))
 
-    print(f"Train set size = {train_set.shape[0]}")
-    print(f"Valid set size = {valid_set.shape[0]}")
-    return train_set, valid_set
+    x_valid_set = np.vstack((x_train[valid_positive_samples], x_train[valid_negative_samples]))
+    y_valid_set = np.hstack((y_train[valid_positive_samples], y_train[valid_negative_samples]))
+
+    print(f"X Train set size = {x_train_set.shape}")
+    print(f"Y Train set size = {y_train_set.shape}\n")
+    print(f"X Valid set size = {x_valid_set.shape}")
+    print(f"Y Valid set size = {y_valid_set.shape}")
+    return x_train_set, y_train_set, x_valid_set, y_valid_set
 
 if __name__ == '__main__':
     #data_exploration()
