fdc02fa6eaf3e515d8b9dce02e1c727727e5e680
Dylan Phelan <dtphelan1@gmail.com>
2020-10-25 19:34:25 -0400

    Changes to baseline generation

--- problem_2/prob2_baseline_model.py
+++ problem_2/prob2_baseline_model.py
@@ -31,14 +31,13 @@ y_tr_M = np.loadtxt(os.path.join(DATA_PATH, 'y_train_set.csv'), delimiter=',')
 y_va_N = np.loadtxt(os.path.join(DATA_PATH, 'y_valid_set.csv'), delimiter=',')
 
 
-def hyperparameterSelection(plot=False, pickle_it=True):
+def hyperparameterSelection(x_tr_M784, y_tr_M, x_va_N784, y_va_N, max_iter=1000):
     """ Iterate over our C_grid to see which hyperparameter gives us the best performance
     """
     C_grid = np.logspace(-9, 6, 31)
     # NOTE: This was the optimal C-value previously; saving for posterity
     # C_grid = np.asarray([0.31622776601683794])
     solver = 'lbfgs'
-    max_iter = 1000  # Fixed per instructions
 
     lr_models_c = list()
 
@@ -63,26 +62,105 @@ def hyperparameterSelection(plot=False, pickle_it=True):
         bce_tr_lr_i.append(calc_mean_binary_cross_entropy_from_probas(y_tr_M, yproba1_tr_M))
         bce_va_lr_i.append(calc_mean_binary_cross_entropy_from_probas(y_va_N, yproba1_va_N))
 
-    # Conditionally plot the model
-    if plot:
-        plot_error_over_c(C_grid, error_tr_lr_c, error_va_lr_c, 'output-figures/ErrorRateCValues_baseline.png')
-        plot_loss_over_c(C_grid, bce_tr_lr_i, bce_va_lr_i, 'output-figures/LogLossCValues_baseline.png')
+    plot_error_over_c(C_grid, error_tr_lr_c, error_va_lr_c, 'output-figures/ErrorRateCValues_baseline.png')
+    plot_loss_over_c(C_grid, bce_tr_lr_i, bce_va_lr_i, 'output-figures/LogLossCValues_baseline.png')
 
-    min_c_index = np.argmin(error_va_lr_c)
-    min_c = C_grid[min_c_index]
-    print("min_c", min_c)
+    optimal_c_index = np.argmin(error_va_lr_c)
+    optimal_c = C_grid[optimal_c_index]
+    print("optimal_c", optimal_c)
 
-    chosen_model = lr_models_c[min_c_index]
+    # Pickle_and_save optimal model
+    chosen_model = lr_models_c[optimal_c_index]
     with Path(os.path.join(PICKLE_DIR, PICKLE_ORIGINAL_MODEL)).open('wb') as pickle_file:
         pickle.dump(chosen_model, pickle_file)
-    return chosen_model
+    return (optimal_c, chosen_model)
+
+
+def plot_loss_over_iters(iterations, iter_step, bce_tr_lr_i, bce_va_lr_i, filepath):
+    """ Helper plotter for log-loss over iteration counts
+    """
+    fig, ax = plt.subplots()
+
+    # Set up the Log Loss subplot
+    ax.plot(np.arange(1, iterations + 1, iter_step), bce_tr_lr_i, 'b.-', label='train')
+    ax.plot(np.arange(1, iterations + 1, iter_step), bce_va_lr_i, 'r.-', label='valid')
+    ax.set_title('Log Loss over iterations')
+    ax.set_ylabel('loss')
+    ax.set_xlabel("number of iterations")
+    ax.legend()
+    # plt.savefig('output-figures/LogLossIterations.png')
+    plt.savefig(filepath)
+    plt.show()
+
+
+def plot_error_over_iters(iterations, iter_step, error_tr_lr_i, error_va_lr_i, filepath):
+    """ Helper plotter for error over iteration counts
+    """
+    fig, ax = plt.subplots()
+
+    # Set up the Error rate subplot
+    ax.plot(np.arange(1, iterations + 1, iter_step), error_tr_lr_i, 'b:', label='train')
+    ax.plot(np.arange(1, iterations + 1, iter_step), error_va_lr_i, 'r:', label='valid')
+    ax.set_title('Error Rate over iterations')
+    ax.set_ylabel('error')
+    ax.set_xlabel("number of iterations")
+    ax.legend()
+    # plt.savefig('output-figures/ErrorRateIterations.png')
+    plt.savefig(filepath)
+    plt.show()
+
+
+def lossAssessment(x_tr_M784, y_tr_M, x_va_N784, y_va_N, C=1e6):
+    solver = 'lbfgs'
+    iterations = 40
+    iter_step = 1
+
+    lr_models_i = list()
+
+    error_tr_lr_i = list()
+    error_va_lr_i = list()
+
+    bce_tr_lr_i = list()
+    bce_va_lr_i = list()
+
+    for i in range(0, iterations, iter_step):
+        lr_i = sklearn.linear_model.LogisticRegression(max_iter=i+1, C=C, solver=solver)
+        lr_i.fit(x_tr_M784, y_tr_M)  # Part b
+        lr_models_i.append(lr_i)
+
+        yproba1_tr_M = lr_i.predict_proba(x_tr_M784)[:, 1]  # The probability of predicting class 1 on the training set
+        yproba1_va_N = lr_i.predict_proba(x_va_N784)[:, 1]  # The probability of predicting class 1 on the validation set
+
+        error_tr_lr_i.append(sklearn.metrics.zero_one_loss(y_tr_M, yproba1_tr_M >= 0.5))
+        error_va_lr_i.append(sklearn.metrics.zero_one_loss(y_va_N, yproba1_va_N >= 0.5))
+
+        bce_tr_lr_i.append(calc_mean_binary_cross_entropy_from_probas(y_tr_M, yproba1_tr_M))
+        bce_va_lr_i.append(calc_mean_binary_cross_entropy_from_probas(y_va_N, yproba1_va_N))
+    # Plot error & losses
+
+    plot_loss_over_iters(iterations, iter_step, bce_tr_lr_i, bce_va_lr_i, 'output-figures/LogLossIterations_baseline.png')
+    plot_error_over_iters(iterations, iter_step, error_tr_lr_i, error_va_lr_i, 'output-figures/ErrorRateIterations.png_baseline.png')
+
+    # Plot ROC curve
+    plot_roc_curve(y_va_N, yproba1_va_N, 'output-figures/ROC_curve_baseline_iteration_loss.png')
+
+    # Find C value with best bce & error rates
+    best_error_ind = error_va_lr_i.index(min(error_va_lr_i))
+    best_bce_ind = bce_va_lr_i.index(min(bce_va_lr_i))
+    print(f"number of iterations with lowest error = {best_error_ind + 1}")
+    print(f"number of iterations with lowest bce = {best_bce_ind + 1}")
+    # Use the best BCE index as our optimal value
+    optimal_iter = best_bce_ind + 1
+    chosen_model = lr_models_i[best_bce_ind]
+    return (optimal_iter, chosen_model)
 
 
 if __name__ == '__main__':
-    # For consistent splits, use a random_state param
-    random_state = 100
-    split_into_train_and_valid(n_valid_samples=2000, random_state=random_state)
-    optimal_model = hyperparameterSelection(plot=True, pickle_it=True)
+    # Uncomment these two lines if you need to generate a fresh train-validation
+    # random_state = 100
+    # split_into_train_and_valid(n_valid_samples=2000, random_state=random_state)
+    (optimal_iters, _) = lossAssessment(x_tr_M784, y_tr_M, x_va_N784, y_va_N)
+    (_, optimal_model) = hyperparameterSelection(x_tr_M784, y_tr_M, x_va_N784, y_va_N, max_iter=optimal_iters)
     yproba1_tr_M = optimal_model.predict_proba(x_tr_M784)[:, 1]
     yproba1_va_N = optimal_model.predict_proba(x_va_N784)[:, 1]
     plot_roc_curve(y_va_N, yproba1_va_N, 'output-figures/ROC_curve_baseline.png')
